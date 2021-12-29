9403015851db1b05b040fa20729b5750d45711ec
iumohammady <96796033+iumohammady@users.noreply.github.com>
2021-12-29 11:04:02 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/notashwin/VCPlayer /VCPlayer
+  git clone https://github.com/iumohammady/VCPlayer /VCPlayer
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/notashwin/VCPlayer -b $BRANCH /VCPlayer
+  git clone https://github.com/iumohammady/VCPlayer -b $BRANCH /VCPlayer
 fi
 cd /VCPlayer
 pip3 install -U -r requirements.txt
